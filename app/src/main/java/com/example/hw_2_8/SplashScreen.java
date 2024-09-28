579815ee4a2c69fc09740ccb68027514a1ba2bf0
Alis09 <alishersagynbekov4@gmail.com>
2024-09-28 17:54:52 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw_2_8/SplashScreen.java
@@ -0,0 +1,22 @@
+package com.example.hw_2_8;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 5000);
+    }
+}
